8bce5b1497c8190d9e9a29bf36c416cdac6afd97
kkig <kigoshi.kanazawa@gmail.com>
2022-10-05 18:11:19 +0900

    Add explanation comments

--- myApp.js
+++ myApp.js
@@ -8,9 +8,11 @@ console.log('Hello World');
 // });
 
 app.get('/', function (req, res) {
+  // Respond w/ html file
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// Mount middleware to serve static assets
 app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
