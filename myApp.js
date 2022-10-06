42ba44065b3b536dc34743a39965bbe1c362299c
kkig <kigoshi.kanazawa@gmail.com>
2022-10-06 19:23:24 +0900

    Get Route Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+//
 // Mount middleware to serve static assets
 // app.use() will be executed for all the requests because default path is "/".
 app.use('/public', express.static(__dirname + '/public'));
@@ -40,6 +41,7 @@ app.get('/json', function (req, res) {
   });
 });
 
+//
 // Chain middleware and get time
 function middleware(req, res, next) {
   req.time = new Date().toString();
@@ -56,6 +58,13 @@ function middleware(req, res, next) {
 
 app.get('/now', middleware, (req, res) => {
   res.send({time: req.time});
+});
+
+//
+// Route param
+app.get('/:word/echo', function (req, res) {
+  const word = req.params.word;
+  res.send({echo: word});
 });
 
 module.exports = app;
